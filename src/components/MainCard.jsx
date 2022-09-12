a42a16154db32a34c6cb0ef7fd29d7cd3f0a0631
Philippe <sevasdoge.01@gmail.com>
2022-09-12 15:33:41 -0400

    First commit

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,12 +1,13 @@
 import LocationDesc from './LocationDesc.jsx'
 import background from '../assets/backgroundRyM.png'
+import logo from '../assets/RyMLogo.jpg'
 export default function MainCard() {
   
   return (
     <>
     <div className="header-container">
       <img src={background} alt="background" className="background"/>
-        <img src="./src/assets/RyMLogo.png" alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" />
         <div className="header-card">
           <h1 className="header-title">Universal Civil Registration</h1>
         </div>
