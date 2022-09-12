2d3317288d6303a2a169eb5b472d842da26ca39a
Philippe <sevasdoge.01@gmail.com>
2022-09-12 15:15:02 -0400

    First commit

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -4,8 +4,8 @@ export default function MainCard() {
   return (
     <>
     <div className="header-container">
-      <img src="./src/assets/backgroundRyM.png" alt="background" className="background"/>
-        <img src="./src/assets/RyMLogo.png" alt="logo" className="logo" />
+      <img src="../src/assets/backgroundRyM.png" alt="background" className="background"/>
+        <img src=".,/src/assets/RyMLogo.png" alt="logo" className="logo" />
         <div className="header-card">
           <h1 className="header-title">Universal Civil Registration</h1>
         </div>
